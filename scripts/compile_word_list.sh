52cccec3c4290412b1ddac7c6f8d910f8f27165b
microhacker <microhacker07@users.noreply.github.com>
2021-07-10 05:56:55 -0400

    Removes rough.txt after compiling words.txt

--- scripts/compile_word_list.sh
+++ scripts/compile_word_list.sh
@@ -6,3 +6,5 @@ cat additional_words.txt >> rough.txt
 # Pain in the ass to why there are doubles in a file after running uniq
 # But this removes carraige returns that make some words 'unique'
 tr -d '\r' < rough.txt | tr [:upper:] [:lower:] | sort | uniq > words.txt
+
+rm ./rough.txt
