22557aedfc890366c4229b2892b12d87ff682d1c
microhacker <microhacker07@users.noreply.github.com>
2021-07-10 04:24:51 -0400

    removes duplicates in words.txt Also added some additional words to that are in the game and not in the words.txt file

--- scripts/compile_word_list.sh
+++ scripts/compile_word_list.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+
 curl $(cat sources.txt) > rough.txt
-tr A-Z a-z < rough.txt | sort | uniq > words.txt
+cat additional_words.txt >> rough.txt
+
+# Pain in the ass to why there are doubles in a file after running uniq
+# But this removes carraige returns that make some words 'unique'
+tr -d '\r' < rough.txt | tr [:upper:] [:lower:] | sort | uniq > words.txt
